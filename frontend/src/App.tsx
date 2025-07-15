275947b07bfef90b9b60b8767b7d747bb57ffe1b
LuisParmigiani <parmigianiluis04@gmail.com>
2025-07-15 17:23:36 -0300

    borre carpetas para unir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import NavbarCarouselHomepage from './components/Navbar-CarouselHomepage/Navbar-CarouselHomepage.js';
+import Homepage from './components/Homepage/Homepage.js';
 import './App.css';
-import HomepagePhrase from './components/HomepagePhrase/HomepagePhrase.js';
-import HomepageCards from './components/HomepageCards/HomepageCards.js';
-import CommentsFooterHomepage from './components/Comments-FooterHomepage/Comments-FooterHomepage.js';
+
 import About from './pages/about/about.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
-  const id_user = undefined; //no se si anda
-
   return (
     <>
       <Router>
@@ -16,10 +12,7 @@ function App() {
             path="/"
             element={
               <>
-                <NavbarCarouselHomepage id={id_user} />
-                <HomepagePhrase />
-                <HomepageCards />
-                <CommentsFooterHomepage />
+                <Homepage />
               </>
             }
           />
---
+++ frontend/src/components/stars/Stars.tsx
@@ -0,0 +1,32 @@
+import style from './Stars.module.css';
+
+type Props = {
+  cant: number;
+};
+function Stars({ cant }: Props) {
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    if (i < cant) {
+      stars.push(
+        <img
+          key={`full-${i}`}
+          className={style.star}
+          src="./images/full-star.png"
+          alt="estrella llena"
+        />
+      );
+    } else {
+      stars.push(
+        <img
+          key={`empty-${i}`}
+          className={style.star}
+          src="./images/empty-star.png"
+          alt="estrella vacía"
+        />
+      );
+    }
+  }
+  return <div className={style.starsContainer}>{stars}</div>;
+}
+
+export default Stars;
