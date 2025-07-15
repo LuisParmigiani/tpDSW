e94bcfe73d50fff4fbcb6241e82345cde3d9b778
LuisParmigiani <parmigianiluis04@gmail.com>
2025-07-15 15:53:01 -0300

    cosas que no guarde

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,39 +2,30 @@ import NavbarCarouselHomepage from './components/Navbar-CarouselHomepage/Navbar-
 import './App.css';
 import HomepagePhrase from './components/HomepagePhrase/HomepagePhrase.js';
 import HomepageCards from './components/HomepageCards/HomepageCards.js';
-import Comments from './components/Comments/Comments.js';
-import About from './pages/about/about.js'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CommentsFooterHomepage from './components/Comments-FooterHomepage/Comments-FooterHomepage.js';
+import About from './pages/about/about.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   const id_user = undefined; //no se si anda
 
   return (
-    
     <>
-    <Router>
-
-      <Routes>
-        <Route path="/" element={
-          <>
-            <NavbarCarouselHomepage id={id_user} />
-            <HomepagePhrase />
-            <HomepageCards />
-            <Comments />
-          </>  
-        } />
-        <Route path="/about" element={<About/>}/>
-       
-
-        
-      </Routes>
-    </Router>
-      
-    <>
-      <NavbarCarouselHomepage id={id_user} />
-      <HomepagePhrase />
-      <HomepageCards />
-      <CommentsFooterHomepage />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <NavbarCarouselHomepage id={id_user} />
+                <HomepagePhrase />
+                <HomepageCards />
+                <CommentsFooterHomepage />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
