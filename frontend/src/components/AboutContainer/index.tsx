2f9e8315006acc6fd78230e2850d5f5dabca6dbf
juabn <gaboteamodemasiado@gmail.com>
2025-07-15 14:59:15 -0300

    componente containerabout, pagina about y modif el navbar

--- frontend/src/components/AboutContainer/index.tsx
+++ frontend/src/components/AboutContainer/index.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 import styles from "./AboutContainer.module.css";
+type Props = {
+  isAlignedRight: boolean;
+  imageUrl: string;
+  title: string;
+  text: string;
+};
 
-function AboutContainer() {
+
+function AboutContainer({isAlignedRight,imageUrl,title,text}: Props) {
+    let aboutTextTitle = styles.aboutTextTitleRight;
+    let aboutTextDescription = styles.aboutTextDescriptionRight;
+    let aboutImage = styles.aboutImageRight;
+    let aboutText = styles.aboutTextRight;
+
+    function alignRight(isAlignedRight:boolean) {
+        if (!isAlignedRight){
+            aboutTextTitle = styles.aboutTextTitleLeft;
+            aboutTextDescription = styles.aboutTextDescriptionLeft;
+            aboutImage = styles.aboutImageLeft;
+            aboutText = styles.aboutTextLeft;
+        }
+    }
+    alignRight(isAlignedRight);
+
     return(
         <div className={styles.aboutContainer}>
             <div className={styles.aboutContent}>
-                <div className={styles.aboutText}>
-                    <h1 className = {styles.aboutTextTitle}>Sobre nosotros</h1>
-                    <p className={styles.aboutTextDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime expedita aliquid excepturi natus doloribus eligendi odio voluptas blanditiis, recusandae, fugit sint exercitationem iusto similique perspiciatis beatae eius magnam voluptates amet aliquam. Libero, ducimus aperiam pariatur impedit incidunt consequatur esse accusantium numquam ex eius perferendis ad debitis aliquid odit! Laborum, saepe?</p>
+                <div className={aboutText}>
+                    <h1 className  ={aboutTextTitle} >{title}</h1>
+                    <p className={aboutTextDescription}>{text}</p>
+                    
                 </div>
-                <div className={styles.aboutImage}>
-                    <img className={styles.aboutImageContent} src="/images/nosotros-about.jpg" alt="Foto de nosotros"/>
+                <div className={aboutImage}>
+                    <img className={styles.aboutImageContent} src={imageUrl} alt="Foto about us"/>
                 </div>
             </div>
         </div>
