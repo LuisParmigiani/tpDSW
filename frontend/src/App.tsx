475b259c118fa86621520da6aadf1a4836e38248
Santiago <santiago.malet.12@gmail.com>
2025-08-02 15:03:32 -0300

    login, inicio, y registro en tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import Borrower from './components/Borrower/Borrower.js';
 import Homepage from './components/Homepage/Homepage.js';
+import Login from './pages/about/App_login.js';
+import Recovery from './pages/about/recovery.js';
+import Registration from './pages/about/registration.js';
 import './App.css';
 
 import About from './pages/about/about.js';
@@ -24,6 +27,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/borrower/:id" element={<Borrower id={13} />} />
           <Route path="/Servicios" element={<Servicios />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/registration" element={<Registration />} />
         </Routes>
       </Router>
     </>
