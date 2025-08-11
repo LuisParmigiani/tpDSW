e6be7acde957c7c4230feb54a9bdc02e8f9049aa
Agustin Dana <agusidana04@gmail.com>
2025-08-11 20:32:25 -0300

    dejo más lindo servicios

--- frontend/src/components/Botones/BotonServicios.tsx
+++ frontend/src/components/Botones/BotonServicios.tsx
@@ -2,8 +2,13 @@ import './../../index.css';
 type Props = {
   texto: string;
   contactar?: () => void;
+  estilosExtras?: string;
 };
-export default function BotonServicios({ texto, contactar }: Props) {
+export default function BotonServicios({
+  texto,
+  contactar,
+  estilosExtras,
+}: Props) {
   const handeClick = () => {
     if (contactar) contactar();
     else alert('Botón presionado');
@@ -12,8 +17,9 @@ export default function BotonServicios({ texto, contactar }: Props) {
     <button
       onClick={handeClick}
       className={
-        'bg-naranja-1 text-white text-center py-2 px-4 rounded-md hover:bg-white ' +
-        'hover:border-naranja-1 hover:border-2 hover:text-naranja-1 hover:text-primary transition duration-300 '
+        'bg-naranja-1 text-white text-center  py-3 px-5 rounded-md hover:bg-white text-xl font-semibold ' +
+        'border-naranja-1 border-2 hover:text-naranja-1 hover:text-primary transition duration-300 ' +
+        estilosExtras
       }
     >
       {texto}
