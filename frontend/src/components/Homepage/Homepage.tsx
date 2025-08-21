cee9708692e95119068952fd41a530e8a963ee70
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-08-21 18:55:22 -0300

    8

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import Navbar from '../Navbar/Navbar';
+import Navbar from '../Navbar/Navbar.tsx';
 import Carousel from '../Carousel/Carousel.js';
 import Comments from '../comments/Comments';
 import Footer from '../Footer/Footer';
