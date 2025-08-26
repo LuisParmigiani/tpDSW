ef5525b47743748fe3422010d5bcdb36555ffc50
Santiago <santiago.malet.12@gmail.com>
2025-08-26 20:54:23 -0300

    parte de la recuperación de password por mail

--- frontend/src/services/usuariosApi.ts
+++ frontend/src/services/usuariosApi.ts
@@ -37,4 +37,7 @@ export const usuariosApi = {
   update: (id: string, data: EntityData) => api.put(`/usuario/${id}`, data),
   delete: (id: string) => api.delete(`/usuario/${id}`),
   login: (data: EntityData) => api.get('/usuario/login', { params: data }),
+  recoverPassword: (data: EntityData) => api.post('/usuario/recuperar', data),
+  validateRecoveryCode: (data: EntityData) =>
+    api.post('/usuario/validar-codigo', data),
 };
