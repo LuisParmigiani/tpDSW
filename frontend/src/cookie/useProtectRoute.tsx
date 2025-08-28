93ff842184f7c2d1cc28e4ba21e92e5e4e68faac
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-08-28 16:00:48 -0300

    consol.log

--- frontend/src/cookie/useProtectRoute.tsx
+++ frontend/src/cookie/useProtectRoute.tsx
@@ -8,18 +8,20 @@ type Rol = 'cliente' | 'prestador';
 export const useProtectRoute = (rolesPermitidos?: Rol[]) => {
   const { usuario, loading } = useAuth();
   const navigate = useNavigate();
-
+  console.log('useProtectRoute renderizado');
   useEffect(() => {
     if (loading) return; // Esperar a que termine de cargar
 
     // Si no hay usuario, redirigir al inicio
     if (!usuario) {
+      console.log('useProtectRoute: usuario no autenticado');
       navigate('/');
       return;
     }
 
     // Si se especificaron roles y el usuario no los tiene
     if (rolesPermitidos && !rolesPermitidos.includes(usuario.rol)) {
+      console.log('useProtectRoute: usuario no tiene acceso');
       navigate('/'); // Redirigir al inicio
       return;
     }
@@ -29,6 +31,8 @@ export const useProtectRoute = (rolesPermitidos?: Rol[]) => {
 };
 
 export const useRoleReturn = () => {
+  console.log('useRoleReturn renderizado');
   const { usuario } = useAuth();
+  console.log('useRoleReturn: usuario', usuario);
   return usuario?.rol ?? '';
 };
