73c7d9093eccd23d726265b96656f52024df7150
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-08-28 16:00:48 -0300

    consol.log

--- frontend/src/cookie/useAuth.tsx
+++ frontend/src/cookie/useAuth.tsx
@@ -9,7 +9,7 @@ interface User {
 const useAuth = () => {
   const [usuario, setUsuario] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
-
+  console.log('useAuth renderizado');
   const verificarAuth = async () => {
     try {
       const response = await fetch('/api/auth/verificar', {
@@ -22,9 +22,12 @@ const useAuth = () => {
       } else {
         setUsuario(null);
       }
+      console.log('useAuth entre al try');
     } catch {
+      console.log('useAuth entre al catch');
       setUsuario(null);
     } finally {
+      console.log('useAuth entre al finally');
       setLoading(false);
     }
   };
