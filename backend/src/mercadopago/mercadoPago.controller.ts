dade5a636dc713f6ce8290ff6dc3054789f3ed31
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-08-28 16:49:54 -0300

    MP

--- backend/src/mercadopago/mercadoPago.controller.ts
+++ backend/src/mercadopago/mercadoPago.controller.ts
@@ -35,19 +35,15 @@ mercadoPago.post('/', async (req: Request, res: Response) => {
           },
         ],
         back_urls: {
-          success: 'https://reformix.site/turnHistory/success',
-          failure: 'https://reformix.site/turnHistory/failure',
-          pending: 'https://reformix.site/turnHistory/pending',
+          success: 'https://reformix.site/historial/success',
+          failure: 'https://reformix.site/historial/failure',
+          pending: 'https://reformix.site/historial/pending',
         },
         auto_return: 'approved',
         notification_url:
           'https://backend-patient-morning-1303.fly.dev/webhooks/mercadopago/cambio',
         external_reference: turno || undefined,
         marketplace: 'Reformix',
-        marketplace_fee: 5, // comisión que retiene tu cuenta principal (ARS)
-        // El collector principal es el vendedor secundario
-        collector_id: '123',
-        payer: 'test_user_792057294485026311@testuser.com',
       } as any,
     });
 
