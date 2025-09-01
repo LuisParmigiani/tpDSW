560753e711c2c6ceb66afc858640fdc6be77ac7b
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-09-01 18:22:39 -0300

    mercado Pago prueba 2

--- frontend/src/services/mercadoPagoApi.ts
+++ frontend/src/services/mercadoPagoApi.ts
@@ -1,11 +1,11 @@
 import { api } from './api';
 import type { EntityData } from './api';
 export const mercadoPagoApi = {
-  create: (data: EntityData) => api.post('/mercadoPago', data),
+  create: (data: EntityData) => api.post('/mercadopago', data),
 
   // OAuth endpoints
   connect: (userId: number) =>
-    api.get('/mercadopago/webhooks/oauth/connect', { params: { userId } }),
+    api.get('/mercadopago/oauth/connect', { params: { userId } }),
   callback: (params: Record<string, string>) =>
     api.get('/mercadopago/oauth/callback', { params }),
   refresh: () => api.post('/mercadopago/oauth/refresh'),
