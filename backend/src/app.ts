b8c88ac57bb9a3d8b7ad0d4163c0017060ca90e6
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-09-01 11:35:48 -0300

    arreglar fly

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,6 +24,7 @@ import authRoutes from './shared/middleware/auth.routes.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Cargar .env solo en desarrollo local
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 const app = express();
 // Como las variables de env son siempre string, tiene que comparar si es igual a 'true', entonces almacena el booleano de js
