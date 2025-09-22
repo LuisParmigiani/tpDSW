f72d39317edd7b2e90bd30ef6ed8cfdc84631a31
Luis Parmigiani <parmigianiluis04@gmail.com>
2025-09-22 12:06:38 -0300

    cambio en shcema

--- backend/src/usuario/usuario.schemas.ts
+++ backend/src/usuario/usuario.schemas.ts
@@ -61,7 +61,7 @@ export const usuarioBaseSchema = z.object({
   telefono: z
     .string()
     .regex(
-      /^[1-9][0-9]{7,9}$/,
+      /^[1-9][0-9]{8,12}$/,
       'El número de documento debe tener entre 8 y 10 dígitos, no puede empezar con 0'
     )
     .describe('Número de teléfono')
